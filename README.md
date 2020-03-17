d7d1054c5b267fc26655eefe1cde005c0f40c346
Prince7195 <vijaydeepak7195@outlook.com>
2020-03-17 17:45:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-eavne7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-eavne7)
---
+++ index.js
@@ -0,0 +1,45 @@
+var data = "";
+var mimeType = "";
+var fileName = "";
+document.getElementById("file-input").addEventListener(
+  "change",
+  event => {
+    fileToBase64(event).then(result => {
+      const txt = result.split(",")[1];
+      data = txt;
+    });
+  },
+  false
+);
+
+// Convert file to base64 string
+function fileToBase64(evt) {
+  return new Promise(resolve => {
+    var file = evt.target.files[0];
+    mimeType = file.type;
+    fileName = file.name;
+    var reader = new FileReader();
+    // Read file content on file loaded event
+    reader.onload = function(event) {
+      resolve(event.target.result);
+    };
+
+    // Convert data to base64
+    reader.readAsDataURL(file);
+  });
+}
+
+document.getElementById("download").addEventListener("click", () => {
+  download(fileName, data, mimeType);
+});
+
+function download(name, content) {
+  var link = document.createElement("a");
+  link.style.display = "none";
+  link.href = `data:application/octet-stream;base64,${encodeURIComponent(content)}`;
+  link.download = name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
